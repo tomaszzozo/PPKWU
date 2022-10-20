ac5b452c05132e141792ad1fd6f8a5d5c4efca98
Tomasz Wojtkiewicz <tomaszkuq@gmail.com>
2022-10-20 12:31:28 +0200

    new folder

---
+++ lab2/source/server.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import http.server
+import socketserver
+import os
+import datetime
+
+#print('source code for "http.server":', http.server.__file__)
+
+class web_server(http.server.SimpleHTTPRequestHandler):
+    
+    def do_GET(self):
+
+        print(self.path)
+        
+        if self.path == '/':
+            self.protocol_version = 'HTTP/1.1'
+            self.send_response(200)
+            self.send_header("Content-type", "text/html; charset=UTF-8")
+            self.end_headers()            
+            self.wfile.write(b"Hello World!<br>")
+            now = datetime.datetime.now()
+            self.wfile.write(str(now).encode())
+        else:
+            super().do_GET()
+    
+# --- main ---
+
+PORT = 4080
+
+print(f'Starting: http://localhost:{PORT}')
+
+tcp_server = socketserver.TCPServer(("",PORT), web_server)
+tcp_server.serve_forever()
