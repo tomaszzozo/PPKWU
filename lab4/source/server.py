2ef21be05b2a8e67d619b046e955ba1755acd33d
Tomasz Wojtkiewicz <222616@edu.p.lodz.pl>
2022-11-17 12:56:32 +0100

    it could work

--- lab4/source/server.py
+++ lab4/source/server.py
@@ -20,21 +20,12 @@ class web_server(http.server.SimpleHTTPRequestHandler):
 			self.send_response(200)
 			self.send_header("Content-type", "text/html; charset=UTF-8")
 			self.end_headers()  
-			response = str(query_components['test'])       
+			num1 = int(query_components["num1"])
+			num2 = int(query_components["num2"])
+			response = '{' + f'"sum" : {num1+num2}, "sub" : {num1-num2}, "mul" : {num1*num2}, "div" : {num1//num2}, "mod" : {num1%num2}' + '}'    
 			self.wfile.write(response.encode())
 							
 			return
-			tring = unquote(self.path[6:])
-			lowercase = sum(1 for c in string if c.islower())
-			uppercase = sum(1 for c in string if c.isupper())
-			digits = sum(list(map(lambda x:1 if x.isdigit() else 0,set(string))))
-			special = len(string)-lowercase-uppercase-digits
-			self.protocol_version = 'HTTP/1.1'
-			self.send_response(200)
-			self.send_header("Content-type", "text/html; charset=UTF-8")
-			self.end_headers()  
-			response = '{' + f' "lowercase" : {lowercase}, "uppercase" : {uppercase}, "digits" : {digits}, "special" : {special}' + '}'        
-			self.wfile.write(response.encode())
 		else:
 			super().do_GET()
 # --- main ---
