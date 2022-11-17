bf250749ee4b452e412ec5f07275dcd98ef5dcc7
Tomasz Wojtkiewicz <222616@edu.p.lodz.pl>
2022-11-17 12:49:11 +0100

    checking url parsing

--- lab4/source/server.py
+++ lab4/source/server.py
@@ -3,7 +3,7 @@ import http.server
 import socketserver
 import os
 import datetime
-from urllib.parse import unquote
+from urllib.parse import urlparse, parse_qs
 
 #print('source code for "http.server":', http.server.__file__)
 
@@ -13,8 +13,19 @@ class web_server(http.server.SimpleHTTPRequestHandler):
 
 		print(self.path)
         
-		if self.path.startswith('/?str='):
-			string = unquote(self.path[6:])
+		if self.path.startswith('/?'):
+			parsed_url = urlparse(self.path)
+			parse_qs(parsed_url.query)
+			query_components = dict(qc.split("=") for qc in query.split("&"))
+			self.protocol_version = 'HTTP/1.1'
+			self.send_response(200)
+			self.send_header("Content-type", "text/html; charset=UTF-8")
+			self.end_headers()  
+			response = str(query_components)       
+			self.wfile.write(response.encode())
+							
+			return
+			tring = unquote(self.path[6:])
 			lowercase = sum(1 for c in string if c.islower())
 			uppercase = sum(1 for c in string if c.isupper())
 			digits = sum(list(map(lambda x:1 if x.isdigit() else 0,set(string))))
